3bb15fcc8ea7f6e902729dbc4aba56ac978f9fe9
Cleyton Ferrari <cleyton@w7br.com>
2014-08-13 22:31:33 -0400

    Adicionado os models

---
+++ TDC/TDC.UI.Web/Models/Palestrante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace TDC.UI.Web.Models
+{
+    public class Palestrante
+    {
+        public string Nome { get; set; }
+        public string Twitter { get; set; }
+        public string Bio { get; set; }
+        public string FotoId { get; set; }
+    }
+}
